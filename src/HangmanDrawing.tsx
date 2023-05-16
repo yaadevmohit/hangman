655e559f567862ca9b3ccf5d423f64e431f8ec44
Mohit yadav <mohityadav530@gmail.com>
2023-05-16 18:01:25 -0400

    initial outline

---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,24 @@
+export default function HangmanDrawing(): JSX.Element {
+    return (
+        <>
+        <div style={{position: "relative"}}>
+        <div style={{
+            height: "10px",
+            width: "200px",
+            marginLeft: "120px",
+            background: "black"
+        }}/>
+        <div style={{
+            height: "400px",
+            width: "10px",
+            background: "black",
+            marginLeft: "120px"
+        }}/>
+        <div style={{
+            width: "250px",
+            height: "10px",
+            background: "black"
+        }}></div>
+    </div>
+    </>)
+}
